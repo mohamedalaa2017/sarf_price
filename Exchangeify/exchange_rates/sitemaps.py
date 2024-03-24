4e55f8c085862f16d3301d3ecc350edbc7b02bd7
mohamedalaa <topmohameda@gmail.com>
2024-03-25 01:41:05 +0200

    tring sitemaps

--- Exchangeify/exchange_rates/sitemaps.py
+++ Exchangeify/exchange_rates/sitemaps.py
@@ -1,12 +1,14 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-class ExchangeRatesSitemap(Sitemap):
+class ExchangeRatesSitemap(Sitemap):       
     changefreq = "daily"
     priority = 0.8
 
     def items(self):
+        # Return the actual view names
         return ['index', 'currencies', 'gold', 'fuel', 'news', 'contact', 'about', 'privacy_policy', 'terms_conditions']
 
     def location(self, item):
+        # Reverse the view name to get the URL
         return reverse(item)

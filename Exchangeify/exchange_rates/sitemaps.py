5d70d87bba13d7b55ffb823d532b56f48620e3ff
mohamedalaa <topmohameda@gmail.com>
2024-03-24 16:07:52 +0200

    sitemapes

--- Exchangeify/exchange_rates/sitemaps.py
+++ Exchangeify/exchange_rates/sitemaps.py
@@ -1,29 +1,12 @@
 from django.contrib.sitemaps import Sitemap
-from django.urls import reverse, NoReverseMatch
+from django.urls import reverse
 
 class ExchangeRatesSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.8
 
     def items(self):
-        excluded_patterns = ['/api/']
-        valid_urls = []
-        for name in self.get_named_urlpatterns():
-            try:
-                url = reverse(name)
-                valid_urls.append(url)
-            except NoReverseMatch:
-                pass
-        return valid_urls
+        return ['index', 'currencies', 'gold', 'fuel', 'news', 'contact', 'about', 'privacy_policy', 'terms_conditions']
 
     def location(self, item):
-        return item
-
-    @staticmethod
-    def get_named_urlpatterns():
-        from .urls import urlpatterns
-        named_patterns = []
-        for pattern in urlpatterns:
-            if hasattr(pattern, 'name') and pattern.name:
-                named_patterns.append(pattern.name)
-        return named_patterns
+        return reverse(item)
